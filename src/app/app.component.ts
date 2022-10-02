0be424cd1f6328fa64868f01655d60e30ab955ed
Dmytro Poputnikov <dpoputnikov@gmail.com>
2022-10-02 20:47:03 +0200

    feat: make http req to api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DatingApp_Frontend';
+  users: any;
+
+  $users = this.http.get('https://localhost/api/users');
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (res) => (this.users = res),
+      error: (e) => console.error(e),
+    });
+  }
 }
